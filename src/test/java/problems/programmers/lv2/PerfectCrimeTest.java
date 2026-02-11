056312503b19968fe952398debd4276e7ba5eb7c
Qmini94 <pkm1614@naver.com>
2026-02-11 22:38:16 +0900

    feat: 그리디 반례 추가.

--- src/test/java/problems/programmers/lv2/PerfectCrimeTest.java
+++ src/test/java/problems/programmers/lv2/PerfectCrimeTest.java
@@ -49,4 +49,40 @@ public class PerfectCrimeTest {
 
         assertEquals(-1, pc.solution(info, 6, 1));
     }
+
+    // 그리디 반례: B에게 큰 것부터 주면 틀림
+    @Test
+    void greedyTrap() {
+        PerfectCrime pc = new PerfectCrime();
+
+        int[][] info = {
+                {2, 2}, {2, 2}, {3, 3}
+        };
+
+        assertEquals(3, pc.solution(info, 5, 5));
+    }
+
+    // 물건 1개: B가 가져가는 게 유리
+    @Test
+    void singleItem() {
+        PerfectCrime pc = new PerfectCrime();
+
+        int[][] info = {
+                {3, 1}
+        };
+
+        assertEquals(0, pc.solution(info, 2, 2));
+    }
+
+    // 둘 다 한계가 1이면 아무것도 못 훔침
+    @Test
+    void bothLimitOne() {
+        PerfectCrime pc = new PerfectCrime();
+
+        int[][] info = {
+                {1, 1}
+        };
+
+        assertEquals(-1, pc.solution(info, 1, 1));
+    }
 }
